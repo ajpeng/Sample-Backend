3220b597b8203d3591ed5060789f7689beda823c
craexus <jjpeng@ryerson.ca>
2020-05-17 15:49:18 -0400

    added upsert for csv stock

--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import sqlite3
+from itertools import islice
 
 from models import BranchProduct, Product
 
@@ -13,26 +14,42 @@ STOCK_PATH = os.path.join(ASSETS_DIR, "STOCK.csv")
 DB_PATH = os.path.join(PROJECT_DIR, "db.sqlite")
 
 accepted_branches = ['MM', 'RHSM']
+conn = None
+cursor = None
 
 
 def process_csv_files():
-    products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|",)
-    stock_df = pd.read_csv(filepath_or_buffer=STOCK_PATH, sep="|",)
-    load_products_to_db(stock_df)
+    products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|", )
+    stock_df = pd.read_csv(filepath_or_buffer=STOCK_PATH, sep="|", )
+    load_stocks_to_db(stock_df)
 
 
-def load_products_to_db(stock_df):
-    # for index, row in stock_df.iterrows():
-    #     if str(row['BRANCH']).upper() in accepted_branches:
-    #         print(row['SKU'], row['BRANCH'], row['STOCK'])
+def load_stocks_to_db(stock_df):
     create_connection()
+    for index, row in islice(stock_df.iterrows(), 20):
+        if str(row['BRANCH']).upper() in accepted_branches:
+            insert_stock(row['SKU'], row['BRANCH'], row['PRICE'], row['STOCK'])
 
 
 def create_connection():
+    global conn
+    global cursor
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.fetchall()
-    cursor.execute("""SELECT * FROM products;""")
+
+
+def insert_stock(sku, branch, price, stock):
+    global cursor
+    # Insert if the product does not exits, otherwise update the current stock
+    cursor.execute("""INSERT OR REPLACE INTO branchproducts (id, product_id, branch, stock, price) VALUES 
+                    ((SELECT id FROM branchproducts where product_id = ? ), ?, ?, ?, ?)""",
+                   (sku, sku, str(branch).strip().upper(), price, stock))
+    conn.commit()
+    # print(f'inserted {sku} from {branch}')
+
+
+def close_connection():
+    global conn
     conn.close()
 
 
